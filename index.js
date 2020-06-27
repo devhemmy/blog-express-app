7589b5acd6b587214fff43f07865ddaa920a858a
Ibrahem Bastawi <dev.ibrahembastawi@gmail.com>
2020-06-27 20:19:09 +0200

    adding mongoDB

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
 const app = express();
+//connect to mongo db
+const dbURI =
+  "mongodb+srv://blogadmin:blogadmin5025@nodeblog-vwjvs.mongodb.net/blogsite?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    // listen for requests
+    app.listen(3000);
+    console.log("connected to db");
+  })
+  .catch((err) => console.error(err));
 
-// listen for requests
-app.listen(3000);
+//middlewares and static files
+app.use(express.static("public"));
+app.use(morgan("dev"));
 
 // register view engine
 app.set("view engine", "ejs");
 // app.set('views', 'myviews');
 
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
+  //   const blogs = [
+  //     {
+  //       title: "Yoshi finds eggs",
+  //       snippet: "Lorem ipsum dolor sit amet consectetur",
+  //     },
+  //     {
+  //       title: "Mario finds stars",
+  //       snippet: "Lorem ipsum dolor sit amet consectetur",
+  //     },
+  //     {
+  //       title: "How to defeat bowser",
+  //       snippet: "Lorem ipsum dolor sit amet consectetur",
+  //     },
+  //   ];
+  //   res.render("index", { title: "Home", blogs });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((results) => {
+      res.render("index", { title: "All Blogs", blogs: results });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
 
 app.get("/about", (req, res) => {
